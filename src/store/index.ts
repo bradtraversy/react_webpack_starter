bc09e3f38213e1bcbeeca3717a1f1a9d8442ffff
vladimir.paziuk <vladimir.paziuk@itrexgroup.com>
2018-08-09 16:11:16 +0300

    redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, Store, Middleware } from "redux";
 import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 import reducer from "../reducers";
 import saga from "../sagas";
 
 const sagaMiddleware: SagaMiddleware<Middleware> = createSagaMiddleware();
 
-const store: Store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store: Store = createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(saga);
 
