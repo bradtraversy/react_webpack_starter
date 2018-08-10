056778056df24f8ed04297c1d53536b425642c62
vladimir-paziuk <vladimir.paziuk@gmail.com>
2018-08-10 16:58:11 +0300

    Integrate with react-router v4 (#2)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,20 @@
 import { createStore, applyMiddleware, Store, Middleware } from "redux";
 import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import { routerMiddleware } from "react-router-redux";
+
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { History, createBrowserHistory } from "history";
 
 import reducer from "../reducers";
 import saga from "../sagas";
 
-const sagaMiddleware: SagaMiddleware<Middleware> = createSagaMiddleware();
+export const history: History = createBrowserHistory();
 
-const store: Store = createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+const sagaMiddleware: SagaMiddleware<Middleware> = createSagaMiddleware();
+const middleware: Middleware[] = [ routerMiddleware(history), sagaMiddleware ];
+
+const store: Store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 sagaMiddleware.run(saga);
-
-// const action = type => store.dispatch({type})
 
 export default store;
